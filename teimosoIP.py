c2ab817c066e75dc9b8573bebb804980db499cae
Tiago <you@example.com>
2016-09-27 11:35:40 -0300

    Added first version.

---
+++ teimosoIP.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+# # # # # # # # # # # # # # # # # # # # # 
+# Nofity admins when Internet IP change
+#
+# auto: Tiago de Souza Moraes
+# http://teago.futuria.com.br
+# 
+# Python 2.7 and urlllib2
+# # # # # # # # # # # # # # # # # # # # #
+
+import urllib2
+import time
+from os import path
+from settings import sender, to, subject, smtpserver, username, password, log, again
+
+from smtplib import SMTP_SSL as SMTP
+from email.MIMEText import MIMEText
+
+def sendmail(ipc):
+    print "--- Sendmail" 
+
+    msg = MIMEText(ipc, 'plain')
+    msg['Subject'] = subject
+    msg['From'] = sender
+
+    r = False
+
+    while r == False:
+        """
+            Why while?
+
+            IP change but:
+                smtp server error
+                internet error
+                others
+        """
+
+        try:
+            conn = SMTP(smtpserver)
+            conn.set_debuglevel(False)
+            conn.login(username, password)
+            conn.sendmail(sender, to, msg.as_string())
+            conn.close()
+            r = True # success
+            print '- Success! A e-mail was sent'
+        except:
+            print '- Error sending e-mail...'
+            time.sleep(again) # try again in seconds
+
+
+def write_log(fl,ip):
+    print "- Store new Internet IP"
+    fl = open(log,"w")
+    fl.write(ip)
+    fl.close()
+    return fl
+
+
+def read_log(fl):
+    print "- Get last Internet IP"
+    fl = open(log,"r")
+    content = fl.read()
+    fl.close()
+    return content
+
+
+# get current ip
+def get_current_ip():
+    url = urllib2.urlopen("http://api.ipify.org") # return internet IP plain text
+    r = url.read()
+    url.close()
+    return r
+
+
+# # # # # # # # #
+# main code 
+# # # # # # # # #
+
+print '--- Checking internet IP'
+ipc = get_current_ip()
+
+# check IP change
+if path.isfile(log): # file log exit? first time no exist.
+    ipl = read_log(log) # ipl = last ip stored
+
+    if not ipl == ipc :
+        sendmail(ipc)
+        write_log(log,ipc) # store new IP in log tile.
+    else:
+        print '- Not change'
+
+else: # not last log, sendmail and store ip.
+    print "- Non exist last IP"
+    write_log(log,ipc)
+    sendmail(ipc)
